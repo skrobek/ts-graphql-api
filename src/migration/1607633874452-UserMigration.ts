038d7ed724f82380651341e11ff953495b310553
Pawel Skroban <pskrobek@gmail.com>
2020-12-10 21:58:18 +0100

    Add example migrations and typeorm cli

---
+++ src/migration/1607633874452-UserMigration.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class UserMigration1607633874452 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
--- src/resolvers/UserResolver.ts
+++ src/resolvers/UserResolver.ts
@@ -1,5 +1,6 @@
 import { Resolver, Query, Mutation, Arg } from 'type-graphql';
 import { User } from '../models/User';
+import { UpdateUserInput } from './inputs/UpdateUserInput';
 import { CreateUserInput } from './inputs/UserInput';
 
 @Resolver()
@@ -10,10 +11,29 @@ export class UserResolver {
     return User.find()
   }
 
+  @Query(() => User)
+  user(@Arg("id") id: string) {
+    return User.findOne({ where: { id }});
+  }
+
   @Mutation(() => User)
   async createUser(@Arg("data") data: CreateUserInput) {
     const user = User.create(data);
     await user.save();
+    return user;
+  }
+
+  @Mutation(() => User)
+  async updateUser(@Arg("id") id: string, @Arg("data") data: UpdateUserInput) {
+    const user = await User.findOne({ where: { id }});
+
+    if (!user) {
+      throw new Error(`The user with id: ${id} does not exist!`);
+    }
+
+    Object.assign(user, data);
+    await user.save();
+
     return user;
   }
 }
